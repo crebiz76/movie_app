571e875a04a532654a604b7e1af9a3ddeca1a3de
ToBeNew <crebiz76@gmail.com>
2021-01-10 11:25:10 +0900

    Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import React from 'react';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("constructor");
-  };
-
-  state = {
-    count: 0
+  state={
+    isLoading: true,
+    movies: []
   };
   
-  plus = () => {
-    console.log("add");
-    // this.state.count += 1; // Not working
-    // this.setState({ count: this.state.count + 1 }) // Not recommended
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    console.log("substract");
-    // this.state.count -= 1; // Not working
-    // this.setState({ count: this.state.count - 1 }) // Not recommended
-    this.setState(current => ({count: current.count - 1}));
-  };
-
   componentDidMount(){
-    console.log("component rendered");
-  };
-
-  componentDidUpdate(){
-    console.log("just component updated");
-  };
-
-  componentWillUnmount(){
-    console.log("just component unmounted");
+    setTimeout(() => { this.setState({ isLoading:false }); }, 5000);
   };
 
   render(){
-    console.log("render");
-    return (
-      <div>
-        <h1>I am a class component. </h1>
-        <h2>The number is {this.state.count}.</h2>
-        <button onClick={this.plus}>Plus</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{ isLoading ? "Loading...":"We are ready" }</div>
   }  
 }
 
