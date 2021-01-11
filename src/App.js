86d73a648c7408313b09b04646e09a6f6a33270a
ToBeNew <crebiz76@gmail.com>
2021-01-12 01:33:06 +0900

    Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
   state={
@@ -12,19 +13,44 @@ class App extends React.Component{
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies.data.data.movies);
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies:movies, isLoading: false });
+    // this.setState({ isLoading: false, movies: movies });
     // console.log(movies);
-  }
+  };
 
   componentDidMount(){
-    setTimeout(this.loadState, 1000);
+    // setTimeout(this.loadState, 1000);
     this.getMovies();
   };
 
   render(){
-    const { isLoading } = this.state;
-    return <div>{ isLoading ? "Loading...":"We are ready" }</div>
+    const { isLoading, movies } = this.state;
+    return (
+      <div> 
+        { isLoading ? "Loading...": movies.map( movie => {
+          console.log(movie);
+          return (
+            <Movie
+              key={ movie.id }
+              id={ movie.id }
+              year={ movie.year }
+              title={ movie.title }
+              summary={ movie.summary }
+              poster={ movie.medium_cover_image }
+            />
+        );})        
+        }
+      </div>
+    );
   };
-}
+};
 
 export default App;
