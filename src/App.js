11ca05514c04619ac4b3d4b1022e3fadee4a6831
ToBeNew <crebiz76@gmail.com>
2021-01-17 01:20:33 +0900

    Styles Timelapse

--- src/App.js
+++ src/App.js
@@ -29,14 +29,13 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section className="loader">
+      <section className="container">
         { isLoading 
           ? (<div className="loader">
             <span className="loader__text">Loading...</span>
           </div>)
           : (<div className="movies">
               { movies.map(movie => {
-                // console.log(movie);
                 return (
                   <Movie
                     key={movie.id}
