eb611dd27aae6953894e8dc649b2d48d1579a6ea
ToBeNew <crebiz76@gmail.com>
2021-01-18 01:14:13 +0900

    Sharing Props Between Routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css"
 
 function Navigation() {
     return (
-        <div>
+        <div className="navi__container">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
